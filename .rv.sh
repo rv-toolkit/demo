a9f77de9d3930955446c48031f3af08367aadf5f
Yiyi Wang <shd101wyy@sina.com>
2018-11-05 15:29:13 -0600

    Initial commit

---
+++ .rv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This is running under Ubuntu 16.04
+
+# Replace your C compiler command like `gcc` with `kcc`,
+# Use `-fissue-report` flag to collect errors to `my_errors.json` file.
+kcc -fissue-report=./my_errors.json main.c -o a.out
+
+# Run the compiled program and collect run-time errors to `my_errors.json` file, which
+# will be used next step to generate HTML report.
+./a.out
+
+# Generate HTML report with `rv-html-report` command,
+# and output the HTML report to `report_path`.
+report_path="`pwd`/report"
+touch ./my_errors.json && rv-html-report ./my_errors.json -o $report_path
+
+# Upload your HTML report to RV-Toolkit website with `rv-upload-report` command.
+# Please note that `rv-upload-report` only accepts absolute path of HTML report.
+rv-upload-report $report_path
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+  int x;
+  printf("%d\n", (x = 1) + (x = 2));
+  return 0;
+}
